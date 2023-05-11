53aca2911789c14f38f01d45349bcb1e6fe4042f
정기하 Jeong Ki Ha <67414807+KihaJeong1994@users.noreply.github.com>
2023-05-11 22:18:59 +0900

    Time: 27 ms (97.96%), Space: 45.3 MB (55.90%) - LeetHub

--- 1476-subrectangle-queries/1476-subrectangle-queries.java
+++ 1476-subrectangle-queries/1476-subrectangle-queries.java
@@ -10,11 +10,9 @@ class SubrectangleQueries {
     }
     
     public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(i>=row1 && i<=row2 && j>=col1 && j<=col2){
-                    arr[i][j] = newValue;
-                }
+        for(int i=row1;i<=row2;i++){
+            for(int j=col1;j<=col2;j++){
+                arr[i][j] = newValue;
             }
         }
     }
@@ -23,10 +21,3 @@ class SubrectangleQueries {
         return this.arr[row][col];
     }
 }
-
-/**
- * Your SubrectangleQueries object will be instantiated and called as such:
- * SubrectangleQueries obj = new SubrectangleQueries(rectangle);
- * obj.updateSubrectangle(row1,col1,row2,col2,newValue);
- * int param_2 = obj.getValue(row,col);
- */
