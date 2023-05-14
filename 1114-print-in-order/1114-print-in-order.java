8c8388893490a32dd8a97b08493f03f7525ac8a4
정기하 Jeong Ki Ha <67414807+KihaJeong1994@users.noreply.github.com>
2023-05-14 22:28:38 +0900

    Time: 10 ms (99.64%), Space: 41.6 MB (75.05%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.java
+++ 1114-print-in-order/1114-print-in-order.java
@@ -4,32 +4,28 @@ class Foo {
     public Foo() {
     }
 
-    public void first(Runnable printFirst) throws InterruptedException {
-        synchronized(this){
-            printFirst.run();
-            done1=true;
-            notifyAll();
-        }
-        
+    public synchronized void first(Runnable printFirst) throws InterruptedException {
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        done1 = true;
+        notifyAll();
     }
 
-    public void second(Runnable printSecond) throws InterruptedException {
-        synchronized(this){
-            while(!done1){
-                wait();
-            }
-            printSecond.run();
-            done2=true;
-            notifyAll();
+    public synchronized void second(Runnable printSecond) throws InterruptedException {
+        while(!done1){
+            wait();
         }
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        done2 = true;
+        notifyAll();
     }
 
-    public void third(Runnable printThird) throws InterruptedException {
-        synchronized(this){
-            while(!done2){
-                wait();
-            }
-            printThird.run();
+    public synchronized void third(Runnable printThird) throws InterruptedException {
+        while(!done2){
+            wait();
         }
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
     }
 }
