9389de257b0d910bcd09d535b9283466cbf79e9c
정기하 Jeong Ki Ha <67414807+KihaJeong1994@users.noreply.github.com>
2023-05-18 16:51:26 +0900

    Time: 0 ms (100.00%), Space: 42.6 MB (48.08%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -4,13 +4,14 @@ class Solution {
         int k = n%2 == 0 ? n/2 : (n-1)/2;
         int rowMax = k;
         int colMax = n%2 ==0? k : k+1;
+        int tmp,tmp2,tmp3 = 0;
         for(int i=0;i<rowMax;i++){
             for(int j=0;j<colMax;j++){
-                int tmp = matrix[j][n-1-i];
+                tmp = matrix[j][n-1-i];
                 matrix[j][n-1-i] = matrix[i][j];
-                int tmp2 = matrix[n-1-i][n-1-j];
+                tmp2 = matrix[n-1-i][n-1-j];
                 matrix[n-1-i][n-1-j] = tmp;
-                int tmp3 = matrix[n-1-j][i];
+                tmp3 = matrix[n-1-j][i];
                 matrix[n-1-j][i] = tmp2;
                 matrix[i][j] = tmp3;
             }
