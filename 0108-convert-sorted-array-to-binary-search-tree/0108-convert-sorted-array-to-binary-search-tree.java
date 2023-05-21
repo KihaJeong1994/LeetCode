39192b6f37cf29af6156f2cf48b177a5ee72a696
정기하 Jeong Ki Ha <67414807+KihaJeong1994@users.noreply.github.com>
2023-05-21 19:30:28 +0900

    Time: 0 ms (100.00%), Space: 43.7 MB (5.31%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
@@ -25,11 +25,6 @@ class Solution {
     private TreeNode getTreeNodeFromTo(int from, int to, int[] nums){
         if(to-from<0) return null;
         if(to-from==0) return new TreeNode(nums[to]);
-        if(to-from==1){
-            TreeNode node = new TreeNode(nums[from]);
-            node.right = new TreeNode(nums[to]);
-            return node;
-        }
         int mid = (from+to)/2;
         TreeNode root = new TreeNode(nums[mid]);
         root.left = getTreeNodeFromTo(from,mid-1, nums);
