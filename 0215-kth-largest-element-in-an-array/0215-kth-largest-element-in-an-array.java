06d13514945eab8d5ea0968af1b347a4c661213a
정기하 Jeong Ki Ha <67414807+KihaJeong1994@users.noreply.github.com>
2023-05-22 21:06:06 +0900

    Time: 27 ms (57.28%), Space: 54.6 MB (12.86%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,49 +1,14 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        TreeNode root = new TreeNode(nums[0]);
-        for(int i=1;i<nums.length;i++){
-            TreeNode node = root;
-            TreeNode newNode = new TreeNode(nums[i]);
-            while(node!=null){
-                if(nums[i]<=node.val){
-                    if(node.left!=null){
-                      node = node.left;  
-                    } else{
-                     node.left = newNode; break;    
-                    }
-                    
-                }else{
-                    if(node.right!=null){
-                      node = node.right;  
-                    } else{
-                     node.right = newNode; break;    
-                    }
-                    
-                }
-                
-            }
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length/2;i++){
+            int tmp = nums[nums.length-1-i];
+            nums[nums.length-1-i] = nums[i];
+            nums[i] = tmp;
         }
-        
-        Stack<TreeNode> stack = new Stack<>();
-        while(true){
-            while(root!=null){
-                stack.push(root);
-                root = root.right;
-            }
-            root = stack.pop();
-            if(--k==0) return root.val;
-            root = root.left;
-        }
-        
+        return nums[k-1];
         
     }
     
-    static class TreeNode{
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode(int val){
-            this.val = val;
-        }
-    }
+    
 }
