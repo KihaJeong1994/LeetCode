f32481713893ab42ed7adea9dede5ee1db85c401
정기하 Jeong Ki Ha <67414807+KihaJeong1994@users.noreply.github.com>
2023-05-23 12:44:14 +0900

    Time: 3 ms (7.75%), Space: 54.3 MB (5.06%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] multiplyForward = new int[nums.length];
+        int[] multiplyBackward = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            if(i==0){
+                multiplyForward[i] = nums[i];
+                multiplyBackward[nums.length-1-i] = nums[nums.length-1-i];
+            }else{
+                multiplyForward[i] = multiplyForward[i-1] * nums[i];
+                multiplyBackward[nums.length-1-i] = multiplyBackward[nums.length-1-i+1] * nums[nums.length-1-i];
+            }
+        }
+        int[] answer = new int[nums.length];
+        for(int i=0; i< nums.length; i++){
+            int forward = i!=0 ? multiplyForward[i-1] : 1;
+            int backward = i!=nums.length-1 ? multiplyBackward[i+1] : 1;
+            answer[i] = forward * backward;
+        }
+        return answer;
+    }
+}
