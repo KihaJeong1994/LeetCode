ee1377ab86f8cb872d5e2da17c08aaf8dac8448d
정기하 Jeong Ki Ha <67414807+KihaJeong1994@users.noreply.github.com>
2023-05-24 12:48:20 +0900

    Time: 0 ms (100.00%), Space: 44 MB (5.53%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,11 +1,9 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
-        int originSum = n*(n+1)/2;
         int sum = 0;
         for(int i : nums){
             sum += i;
         }
-        return originSum-sum;
+        return nums.length*(nums.length+1)/2-sum;
     }
 }
