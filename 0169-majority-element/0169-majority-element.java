7746df5f4bbdd9425af1b3d028b2bbb91dc37fcd
정기하 Jeong Ki Ha <67414807+KihaJeong1994@users.noreply.github.com>
2023-05-26 08:51:20 +0900

    Time: 13 ms (32.43%), Space: 47.1 MB (38.13%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,19 +1,12 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int majorityCnt = 0;
-        int majority = nums[0];
         int majorEnoughCnt = nums.length/2;
         Map<Integer, Integer> map = new HashMap<>();
         for(int n : nums){
             Integer cnt = map.getOrDefault(n, 0);
             map.put(n, ++cnt);
-            if(cnt>majorityCnt){
-                majority = n;
-                majorityCnt = cnt;
-            }
-            if(cnt > majorEnoughCnt) return majority;
+            if(cnt > majorEnoughCnt) return n;
         }
-        return majority;
-        
+        return nums[0];
     }
 }
