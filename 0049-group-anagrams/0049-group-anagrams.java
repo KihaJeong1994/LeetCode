f17abe6300d59e6b4fe0cd6d77e2778f53508e5c
정기하 Jeong Ki Ha <67414807+KihaJeong1994@users.noreply.github.com>
2023-05-25 12:23:18 +0900

    Time: 7 ms (75.22%), Space: 46.9 MB (21.82%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -3,20 +3,20 @@ class Solution {
         Map<String,List<String>> map = new HashMap<>();
         List<List<String>> answerList = new ArrayList<>();
         for(String str : strs){
-            char[] chars = str.toCharArray();
-            Arrays.sort(chars);
-            String sorted = new String(chars);
-            List<String> anagramList = map.get(sorted);
-            if(anagramList==null){
-                anagramList = new ArrayList<>();
-                anagramList.add(str);
-                map.put(sorted, anagramList);
-            }else{
-                anagramList.add(str);
-            }
+            String sorted = getKey(str);
+            List<String> anagramList = map.getOrDefault(sorted,new ArrayList<>());
+            anagramList.add(str);
+            map.put(sorted, anagramList);
         }
         map.forEach((key,val)->answerList.add(val));
         return answerList;
+    }
+
+    private static String getKey(String str) {
+        char[] chars = str.toCharArray();
+        Arrays.sort(chars);
+        String sorted = new String(chars);
+        return sorted;
     }
 
     
