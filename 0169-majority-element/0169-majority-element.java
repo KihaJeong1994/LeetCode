1f528888a45e61c37a9d764d8e5812747654f0ef
정기하 Jeong Ki Ha <67414807+KihaJeong1994@users.noreply.github.com>
2023-05-26 10:08:33 +0900

    Time: 1 ms (99.50%), Space: 48.6 MB (8.24%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,7 +1,21 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        int candidate = nums[0];
+        int majorityCnt = 1;
         int n = nums.length;
-        Arrays.sort(nums);
-        return nums[n/2];
+        for(int i=1 ; i<n ; i++){
+            if(majorityCnt==0){
+                candidate = nums[i];
+                majorityCnt++;
+                continue;
+            }
+            if(nums[i]==candidate){
+                majorityCnt++;
+            }else {
+                majorityCnt--;
+            }
+
+        }
+        return candidate;
     }
 }
