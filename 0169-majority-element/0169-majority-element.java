788ce021c6a15b4150bcf931b302c1f33f782145
정기하 Jeong Ki Ha <67414807+KihaJeong1994@users.noreply.github.com>
2023-05-26 09:01:05 +0900

    Time: 2 ms (66.52%), Space: 48.8 MB (5.32%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -2,19 +2,6 @@ class Solution {
     public int majorityElement(int[] nums) {
         int n = nums.length;
         Arrays.sort(nums);
-        int majorEnoughCnt = nums.length/2;
-        int cnt = 1;
-        for(int i=1; i<n; i++){
-            if(nums[i]==nums[i-1]){
-                cnt++;
-            }else{
-                if(cnt>majorEnoughCnt){
-                    return nums[i-1];
-                }
-                cnt = 1;
-            }
-            
-        }
-        return nums[n-1];
+        return nums[n/2];
     }
 }
