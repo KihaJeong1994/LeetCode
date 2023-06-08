1d7c6f4e6334207be83f58cb73cc7185d68d165e
정기하 Jeong Ki Ha <67414807+KihaJeong1994@users.noreply.github.com>
2023-06-08 12:31:19 +0900

    Time: 2 ms (88.20%), Space: 45.2 MB (33.27%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -1,21 +1,19 @@
 class Solution {
     private List<List<Integer>> answerList = new ArrayList<>();
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        Stack<Integer> path = new Stack<>();
+        List<Integer> path = new ArrayList<>();
         search(0,graph,path);
         return answerList;
     }
     
-    void search(int i, int[][] graph, Stack<Integer> path){
-        path.push(i);
+    void search(int i, int[][] graph, List<Integer> path){
+        path.add(i);
         if(i==graph.length-1){
-            List<Integer> l = new ArrayList<>();
-            path.forEach(p->l.add(p));
-            answerList.add(l);
+            answerList.add(new ArrayList<>(path));
         }else{
             for(int j : graph[i]){
                 search(j,graph, path);
-                path.pop();
+                path.remove(path.size()-1);
             }
         }
         
