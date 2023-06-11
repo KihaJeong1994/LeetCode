6fb4c859d1c77d75bd79617a61c002c548d9c55c
정기하 Jeong Ki Ha <67414807+KihaJeong1994@users.noreply.github.com>
2023-06-11 17:17:31 +0900

    Time: 0 ms (100.00%), Space: 39.2 MB (82.88%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -1,18 +1,11 @@
 class Solution {
     public int countVowelStrings(int n) {
-        int[] dp = new int[6];
-        int sum = 0;
-        for(int i=1;i<=5;i++){
-            dp[i] = 1;
-        }
-        for(int i=2; i<=n;i++){
+        int[] dp = {0,1,1,1,1,1};
+        for(int i=1; i<=n;i++){
             for(int j=1;j<=5;j++){
-                dp[j] = dp[j-1] + dp[j];
+                dp[j] += dp[j-1];
             }
         }
-        for(int i=1;i<=5;i++){
-            sum+=dp[i];
-        }
-        return sum;
+        return dp[5];
     }
 }
