65b323383c723e5c0c748b488ee9f62d1a663195
정기하 Jeong Ki Ha <67414807+KihaJeong1994@users.noreply.github.com>
2023-06-13 21:47:10 +0900

    Time: 6 ms (37.40%), Space: 40.5 MB (63.74%) - LeetHub

---
+++ 1237-find-positive-integer-solution-for-a-given-equation/1237-find-positive-integer-solution-for-a-given-equation.java
@@ -0,0 +1,33 @@
+/*
+ * // This is the custom function interface.
+ * // You should not implement it, or speculate about its implementation
+ * class CustomFunction {
+ *     // Returns f(x, y) for any given positive integers x and y.
+ *     // Note that f(x, y) is increasing with respect to both x and y.
+ *     // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+ *     public int f(int x, int y);
+ * };
+ */
+
+class Solution {
+    public List<List<Integer>> findSolution(CustomFunction customfunction, int z) {
+        List<List<Integer>> answerList = new ArrayList<>();
+        for(int x=1;x<=1000;x++){
+            int start = 1;
+            int end = 1000;
+            while(start<=end){
+                int y = (start+end)/2;
+                int result = customfunction.f(x,y);
+                if(result==z){
+                    answerList.add(Arrays.asList(x,y));
+                    break;
+                }else if(result<z){
+                    start = y +1;
+                }else{
+                    end = y -1;
+                }
+            }
+        }
+        return answerList;
+    }
+}
