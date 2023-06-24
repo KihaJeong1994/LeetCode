b273d3ce424ce0b37830343dc28c5142bea61306
정기하 Jeong Ki Ha <67414807+KihaJeong1994@users.noreply.github.com>
2023-06-24 10:26:22 +0900

    Time: 6 ms (37.67%), Space: 63 MB (47.37%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,31 +1,22 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        List<Integer> lessThanPivot = new ArrayList<>();
-        List<Integer> equalToPivot = new ArrayList<>();
-        List<Integer> greaterThanPivot = new ArrayList<>();
-        
-        for(int num : nums){
-            if(num<pivot){
-                lessThanPivot.add(num);
-            }else if(num==pivot){
-                equalToPivot.add(num);
-            }else{
-                greaterThanPivot.add(num);
+        int low = 0;
+        int high = nums.length-1;
+        int[] answer = new int[nums.length];
+        for(int i=0; i<nums.length; i++){
+            if(low>high) break;
+            if(nums[i]<pivot){
+                answer[low++] = nums[i];
+            }
+            if(nums[nums.length-1-i]>pivot){
+                answer[high--] = nums[nums.length-1-i];
             }
         }
         
-        int[] reOrderedArray = new int[nums.length];
+        while(low<=high){
+            answer[low++] = pivot;
+        }
+        return answer;
         
-        for(int i=0; i<lessThanPivot.size();i++){
-            reOrderedArray[i] = lessThanPivot.get(i);
-        }
-        for(int i=0; i<equalToPivot.size();i++){
-            reOrderedArray[i+lessThanPivot.size()] = equalToPivot.get(i);
-        }
-        for(int i=0; i<greaterThanPivot.size();i++){
-            reOrderedArray[i+lessThanPivot.size()+equalToPivot.size()] = greaterThanPivot.get(i);
-        }
-        
-        return reOrderedArray;
     }
 }
