561eff27d7bf53470e23b33962c24555803594c4
정기하 Jeong Ki Ha <67414807+KihaJeong1994@users.noreply.github.com>
2023-06-30 12:30:59 +0900

    Time: 206 ms (20.55%), Space: 45.8 MB (5.22%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -8,18 +8,21 @@ class Solution {
             cntMap.put(s.charAt(i),++cnt);
             chars[i] = s.charAt(i);
         }
-        // 2. sort char[] by count desc
-        Arrays.sort(chars,(c1,c2)-> {
+        
+        PriorityQueue<Character> pq = new PriorityQueue<>((c1,c2)->{
             if(cntMap.get(c2).equals(cntMap.get(c1))){
                 return c2.compareTo(c1);
             }
             return cntMap.get(c2) - cntMap.get(c1);
         });
+        for(int i=0; i<s.length();i++){
+            pq.add(s.charAt(i));
+        }
 
-        // 3. char[] to String
+        
         StringBuilder sb = new StringBuilder();
-        for(char c : chars){
-            sb.append(c);
+        while (!pq.isEmpty()){
+            sb.append(pq.poll());
         }
         return sb.toString();
     }
