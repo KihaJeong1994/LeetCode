9cc3ad77a6de51892cf149572a4a3ef92f0532c1
정기하 Jeong Ki Ha <67414807+KihaJeong1994@users.noreply.github.com>
2023-08-02 13:13:55 +0900

    Time: 4 ms (83.38%), Space: 55.4 MB (81.63%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -5,15 +5,21 @@ class Solution {
         int right = height.length-1;
 
         while (left<right){
-            maxArea = Math.max((right-left) * Math.min(height[left],height[right]),maxArea);
+            int area = 0;
+            // move the smaller height pointer because move the higher pointer only result in same area or smaller result
+            // since smaller height become the height of area
             if(height[left]<height[right]){
+                area = height[left]*(right-left);
                 left++;
             } else if (height[left]>height[right]) {
+                area = height[right]*(right-left);
                 right--;
             }else {
+                area = height[right]*(right-left);
                 left++;
                 right--;
             }
+            if(area>maxArea) maxArea = area;
         }
         return maxArea;
 
