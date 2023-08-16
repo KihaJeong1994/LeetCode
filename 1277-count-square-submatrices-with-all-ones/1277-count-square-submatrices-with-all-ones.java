2142c9bf482998b5274fcdc772e94055a7821e3c
정기하 Jeong Ki Ha <67414807+KihaJeong1994@users.noreply.github.com>
2023-08-16 13:37:21 +0900

    Time: 6 ms (70.21%), Space: 54 MB (89.36%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -3,27 +3,13 @@ class Solution {
         int answer = 0;
         int m = matrix.length;
         int n = matrix[0].length;
-        int min = Math.min(m, n);
-        boolean[][] dp = new boolean[m][n];
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(matrix[i][j] ==1){
-                    dp[i][j] = true;
-                    answer++;
+                if(matrix[i][j] ==1 && i>=1 && j>=1){
+                    matrix[i][j] = Math.min(Math.min(matrix[i-1][j-1], matrix[i-1][j]), matrix[i][j-1])+1;
                 }
+                answer += matrix[i][j];
             }
-        }
-        for(int len=2; len<=min; len++){
-            boolean[][] newDp = new boolean[m][n];
-            for(int i=0; i<m-len+1; i++){
-                for(int j=0; j<n-len+1; j++){
-                    if(dp[i][j]&&dp[i+1][j]&&dp[i][j+1]&&dp[i+1][j+1]){
-                        newDp[i][j] = true;
-                        answer++;
-                    }
-                }
-            }
-            dp = newDp;
         }
         return answer;
     }
