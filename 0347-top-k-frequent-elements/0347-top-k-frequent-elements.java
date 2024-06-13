4ae8637f92d3e58679c4c58225c8f8eae761b49d
정기하 Jeong Ki Ha <67414807+KihaJeong1994@users.noreply.github.com>
2024-06-13 16:05:08 +0900

    Time: 11 ms (90.79%), Space: 47.8 MB (81.38%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,34 +1,42 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // must be less than O(n log n)
-        // 1. count every num -> O(n)
-        Map<Integer, NumCnt> map = new HashMap<>();
+        // 1. count all nums -> create map with num:cnt
+        Map<Integer, Integer> numCntMap = new HashMap<>();
         for(int num : nums){
-            NumCnt numCnt = map.getOrDefault(num, new NumCnt(num));
-            numCnt.add();
-            map.put(num, numCnt);
+            Integer cnt = numCntMap.getOrDefault(num, 0);
+            cnt++;
+            numCntMap.put(num, cnt);
         }
-        // 2. Create array of class with num and cnt O(n)
-        // 3. sort array by cnt O(n log n)
-        List<Integer> sortedNums = map.values().stream().sorted((a, b) -> b.cnt - a.cnt).map(numCnt -> numCnt.num).toList();
         
-        // 4. return first kth classes
+        // 2. maximum num of cnt = nums.length
+        // make array with length k and put num in kth element
+        List<Integer>[] cntNumsList = new List[nums.length+1];
+        for(int i=0; i< nums.length+1; i++){
+            cntNumsList[i] = new ArrayList<>();
+        }
+        numCntMap.entrySet().forEach((e)->{
+            cntNumsList[e.getValue()].add(e.getKey());
+        });
+        
+        // 3. in reverse order, count k and fill in answer
+        
         int[] answer = new int[k];
-        for(int i=0; i<k; i++){
-            answer[i] = sortedNums.get(i);
+        int end = 0;
+        for(int i = nums.length; i>=0; i--){
+            List<Integer> integers = cntNumsList[i];
+            for(int j=0; j<integers.size(); j++){
+                if(k>0){
+                    answer[end] = integers.get(j);
+                    end++;
+                    k--;
+                }else{
+                    break;
+                }
+            }
+            if(k==0) break;
         }
         return answer;
     }
     
-    static class NumCnt{
-        int num;
-        int cnt;
-        NumCnt(int num){
-            this.num = num;
-            this.cnt = 0;
-        }
-        void add(){
-            this.cnt++;
-        }
-    }
+
 }
