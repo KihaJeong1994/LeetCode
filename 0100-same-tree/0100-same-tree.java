dbd3c95d177d80c69172cd0203730d98385b0cae
정기하 Jeong Ki Ha <67414807+KihaJeong1994@users.noreply.github.com>
2024-07-24 10:09:24 +0900

    Time: 0 ms (100%), Space: 41.1 MB (22.57%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -16,11 +16,7 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p!=null && q !=null){
-            if(p.left == null && p.right == null && q.left == null && q.right == null){
-                return p.val == q.val;
-            }else{
-                return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-            }
+            return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right); 
         }else if(p==null && q==null){
             return true;
         }else{
