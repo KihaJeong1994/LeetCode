8cda71f608ed7b7035f1ffddb93fa81af5003b44
정기하 Jeong Ki Ha <67414807+KihaJeong1994@users.noreply.github.com>
2024-08-06 21:27:25 +0900

    Time: 5 ms (100%), Space: 45 MB (32.04%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,55 +9,19 @@
  */
 
 class Solution {
-    Deque<TreeNode> pAncestorStack = new ArrayDeque<>();
-    boolean foundP = false;
-    boolean foundQ = false;
-    Set<TreeNode> qAncestorSet = new HashSet<>();
-    
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        dfsP(root, p);
-        dfsQ(root, q);
-        TreeNode answer = null;
-        while(!pAncestorStack.isEmpty()){
-            TreeNode ancestor = pAncestorStack.pollFirst();
-            if(qAncestorSet.contains(ancestor)){
-                return ancestor;
+        TreeNode node = root;
+        int pdiff = node.val-p.val;
+        int qdiff = node.val-q.val;
+        while((pdiff>0 && qdiff>0) || (pdiff<0 && qdiff<0)){
+            if(pdiff>0){
+                node = node.left;
+            }else{
+                node = node.right;
             }
+            pdiff = node.val-p.val;
+            qdiff = node.val-q.val;
         }
-        return answer;
-    }
-
-    void dfsP(TreeNode root, TreeNode p){
-        if(root==p){
-            pAncestorStack.offer(root);
-            foundP = true;
-            return;
-        }
-        if(!foundP && root.left!=null){
-            dfsP(root.left, p);
-        }
-        if(!foundP && root.right!=null){
-            dfsP(root.right, p);
-        }
-        if(foundP){
-            pAncestorStack.offer(root);
-        }
-    }
-
-    void dfsQ(TreeNode root, TreeNode p){
-        if(root==p){
-            qAncestorSet.add(root);
-            foundQ = true;
-            return;
-        }
-        if(!foundQ && root.left!=null){
-            dfsQ(root.left, p);
-        }
-        if(!foundQ && root.right!=null){
-            dfsQ(root.right, p);
-        }
-        if(foundQ){
-            qAncestorSet.add(root);
-        }
+        return node;
     }
 }
