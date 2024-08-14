e9e890d2bcb1f3d537937dfbb210e9e7c693dd80
정기하 Jeong Ki Ha <67414807+KihaJeong1994@users.noreply.github.com>
2024-08-14 22:24:05 +0900

    Time: 0 ms (100%), Space: 44.3 MB (58.67%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -14,16 +14,17 @@
  * }
  */
 class Solution {
-        public int kthSmallest(TreeNode root, int k) {
-        LinkedList<TreeNode> stack = new LinkedList<>();
-        while (true){
-            while(root!=null){
-                stack.push(root);
-                root = root.left;
-            }
-            root = stack.pop();
-            if(--k==0) return root.val;
-            root = root.right;
-        }
+    int num = 0;
+    int answer=0;
+    public int kthSmallest(TreeNode root, int k) {
+        inOrder(root,k);
+        return answer;
+    }
+
+    private void inOrder(TreeNode node, int k){
+        if(node.left!=null)inOrder(node.left,k);
+        num++;
+        if(num == k) answer = node.val;
+        if(node.right!=null)inOrder(node.right,k);
     }
 }
