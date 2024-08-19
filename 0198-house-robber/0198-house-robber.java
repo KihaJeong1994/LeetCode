aba034bd5ceea3867845f4f7ed7585fb0c883b7f
정기하 Jeong Ki Ha <67414807+KihaJeong1994@users.noreply.github.com>
2024-08-19 17:52:07 +0900

    Time: 0 ms (100%), Space: 40.9 MB (57.71%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,12 +1,13 @@
 class Solution {
     public int rob(int[] nums) {
-        int length = nums.length;
-        if(length==1) return nums[0];
-        if(length==2) return Math.max(nums[0],nums[1]);
-        nums[1] = Math.max(nums[0],nums[1]);
-        for(int i=2; i<length; i++){
-            nums[i] = Math.max(nums[i-1],nums[i-2]+nums[i]);
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        if(n==1) return dp[0];
+        dp[1] = Math.max(dp[0], nums[1]);
+        for(int i=2; i<n; i++){
+            dp[i] = Math.max(dp[i-2]+nums[i], dp[i-1]);
         }
-        return nums[length-1];
+        return Math.max(dp[n-1], dp[n-2]);
     }
 }
